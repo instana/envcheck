5536dd5e402e785f9306ee21ff34196fc62c06dd
Nathan Fisher <nfisher@junctionbox.ca>
2020-05-11 14:30:50 -0300

    Allow use of offline JSON files

--- cluster/info.go
+++ cluster/info.go
@@ -9,7 +9,91 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+
+	// imports all auth methods for kubernetes go client.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/clientcmd"
 )
+
+// Query is a query interface for the cluster.
+type Query interface {
+	// AllPods returns the list of pods from the related cluster.
+	AllPods() ([]PodInfo, error)
+	Host() string
+}
+
+// New builds a new KubernetesQuery implementation with the given kubeconfig.
+func New(kubeconfig string) (*KubernetesQuery, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &KubernetesQuery{config.Host, clientset}, nil
+}
+
+// KubernetesQuery is a concrete Kubernetes client to query various cluster info.
+type KubernetesQuery struct {
+	host string
+	*kubernetes.Clientset
+}
+
+// Host provides the host info for the cluster.
+func (q *KubernetesQuery) Host() string {
+	return q.host
+}
+
+// AllPods retrieves all pod info from the cluster.
+func (q *KubernetesQuery) AllPods() ([]PodInfo, error) {
+	var cont string
+	var podList []PodInfo
+	namespaces := make(map[string]bool)
+
+	for true {
+		pods, err := q.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{Limit: 100, Continue: cont})
+		if err != nil {
+			return nil, err
+		}
+
+		for _, pod := range pods.Items {
+			info := PodInfo{
+				IsRunning: pod.Status.Phase == v1.PodRunning,
+				Name:      pod.Name,
+				Namespace: pod.Namespace,
+				Host:      pod.Status.HostIP,
+				Owners:    make(map[string]string),
+			}
+			for _, owner := range pod.OwnerReferences {
+				info.Owners[owner.Name] = owner.Kind
+			}
+			namespaces[pod.Namespace] = true
+
+			var containers []ContainerInfo
+			for _, container := range pod.Spec.Containers {
+				containers = append(containers, ContainerInfo{
+					Image: container.Image,
+					Name:  container.Name,
+				})
+			}
+			info.Containers = containers
+			podList = append(podList, info)
+		}
+
+		cont = pods.Continue
+		if cont == "" {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	return podList, nil
+}
 
 // Set provides a set collection for strings.
 type Set map[string]bool
@@ -30,9 +114,9 @@ func (s Set) Contains(item string) bool {
 	return present
 }
 
-// New creates a new cluster index for relevant cluster entities.
-func New() *Index {
-	return &Index{
+// IndexFrom creates a new cluster index for relevant cluster entities.
+func IndexFrom(info *Info) *Index {
+	index := &Index{
 		Containers:   make(Set),
 		DaemonSets:   make(Set),
 		Deployments:  make(Set),
@@ -42,6 +126,9 @@ func New() *Index {
 		Running:      make(Set),
 		StatefulSets: make(Set),
 	}
+	info.Apply(index)
+
+	return index
 }
 
 // Index provides indexes for a number of the cluster entities.
@@ -113,52 +200,6 @@ type Summary struct {
 	StatefulSets int
 }
 
-// AllPods retrieves all pod info from the cluster.
-func AllPods(clientset *kubernetes.Clientset) ([]PodInfo, error) {
-	var cont string
-	var podList []PodInfo
-	namespaces := make(map[string]bool)
-
-	for true {
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{Limit: 100, Continue: cont})
-		if err != nil {
-			return nil, err
-		}
-
-		for _, pod := range pods.Items {
-			info := PodInfo{
-				IsRunning: pod.Status.Phase == v1.PodRunning,
-				Name:      pod.Name,
-				Namespace: pod.Namespace,
-				Host:      pod.Status.HostIP,
-				Owners:    make(map[string]string),
-			}
-			for _, owner := range pod.OwnerReferences {
-				info.Owners[owner.Name] = owner.Kind
-			}
-			namespaces[pod.Namespace] = true
-
-			var containers []ContainerInfo
-			for _, container := range pod.Spec.Containers {
-				containers = append(containers, ContainerInfo{
-					Image: container.Image,
-					Name:  container.Name,
-				})
-			}
-			info.Containers = containers
-			podList = append(podList, info)
-		}
-
-		cont = pods.Continue
-		if cont == "" {
-			break
-		}
-		time.Sleep(100 * time.Millisecond)
-	}
-
-	return podList, nil
-}
-
 // PodApplyable is the interface to receive pod info from a pod collection.
 type PodApplyable interface {
 	Each(PodInfo)
@@ -170,6 +211,8 @@ type Info struct {
 	PodCount int
 	Pods     []PodInfo
 	Version  string
+	Started  time.Time
+	Finished time.Time
 }
 
 // Apply iterates over each pod and yields it to the list of applyables.
--- cmd/envcheckctl/envcheckctl_main.go
+++ cmd/envcheckctl/envcheckctl_main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/user"
@@ -11,9 +12,6 @@ import (
 	"time"
 
 	"github.com/instana/envcheck/cluster"
-	"k8s.io/client-go/kubernetes"
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 var (
@@ -21,37 +19,79 @@ var (
 	Revision string
 )
 
-// Exec is the primary execution for the envcheckctl application.
-func Exec(kubeconfig string) {
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	// create the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
+// QueryLive queries a cluster for the latest data.
+func QueryLive(query cluster.Query) (*cluster.Info, error) {
 	info := &cluster.Info{
-		Name: config.Host,
+		Name:    query.Host(),
+		Started: time.Now(),
 	}
 
-	start := time.Now()
-	log.Printf("envcheckctl=%s, cluster=%v, start=%v\n", Revision, info.Name, start.Format(time.RFC3339))
+	log.Printf("envcheckctl=%s, cluster=%v, start=%v\n", Revision, info.Name, info.Started.Format(time.RFC3339))
 	log.Println("Collecting pod details. Duration varies depending on the cluster.")
-	pods, err := cluster.AllPods(clientset)
+	pods, err := query.AllPods()
 	if err != nil {
-		log.Fatalf("error retrieving pods: %v\n", err)
+		return nil, err
 	}
+	info.Finished = time.Now()
 	info.Pods = pods
-
 	info.PodCount = len(pods)
 
-	index := cluster.New()
-	info.Apply(index)
+	return info, nil
+}
+
+func LoadInfo(r io.Reader) (*cluster.Info, error) {
+	var info cluster.Info
+	dec := json.NewDecoder(r)
+	err := dec.Decode(&info)
+	return &info, err
+}
+
+// Exec is the primary execution for the envcheckctl application.
+func Exec(kubeconfig string, isLive bool, podfile string) {
+	var info *cluster.Info
+
+	// if podfile is set, disable live query
+	isLive = podfile == ""
+
+	if isLive {
+		query, err := cluster.New(kubeconfig)
+		if err != nil {
+			log.Fatalf("error initialising cluster query: %v\n", err)
+		}
+
+		info, err = QueryLive(query)
+		if err != nil {
+			log.Fatalf("error retrieving cluster info: %v\n", err)
+		}
+
+		filename := fmt.Sprintf("cluster-info-%d.json", time.Now().UTC().Unix())
+		w, err := os.Create(filename)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		enc := json.NewEncoder(w)
+		err = enc.Encode(info)
+		w.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("podfile=%s", filename)
+	} else if podfile != "" {
+		r, err := os.Open(podfile)
+		info, err = LoadInfo(r)
+		r.Close()
+		if err != nil {
+			log.Fatalf("error loading cluster info: %v\n", err)
+		}
+		log.Printf("envcheckctl=%s, cluster=%v, podfile=%v\n", Revision, info.Name, podfile)
+	} else {
+		fmt.Println("Either a podfile must be provided or live must be set to true")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	index := cluster.IndexFrom(info)
 	summary := index.Summary()
 
 	log.Printf("pods=%d, running=%d, nodes=%d, containers=%d, namespaces=%d, deployments=%d, daemonsets=%d, statefulsets=%d, duration=%v\n",
@@ -63,7 +103,7 @@ func Exec(kubeconfig string) {
 		summary.Deployments,
 		summary.DaemonSets,
 		summary.StatefulSets,
-		time.Now().Sub(start))
+		info.Finished.Sub(info.Started))
 
 	size := cluster.AgentSize(summary)
 	log.Printf("sizing=instana-agent cpurequests=%s cpulimits=%s memoryrequests=%s memorylimits=%s heap=%s\n",
@@ -72,38 +112,26 @@ func Exec(kubeconfig string) {
 		size.MemoryRequest,
 		size.MemoryLimit,
 		size.Heap)
-
-	w, err := os.Create(fmt.Sprintf("cluster-info-%d.json", time.Now().UTC().Unix()))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer w.Close()
-
-	enc := json.NewEncoder(w)
-	enc.SetIndent("", "  ")
-	err = enc.Encode(info)
-	if err != nil {
-		log.Fatalln(err)
-	}
 }
 
 func main() {
 	var kubeconfig string
+	var podfile string
+	var isLive bool
 
-	if home := homeDir(); home != "" {
-		flag.StringVar(&kubeconfig, "kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		flag.StringVar(&kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
-	}
+	flag.BoolVar(&isLive, "live", true, "retrieve pods from cluster")
+	flag.StringVar(&podfile, "podfile", "", "podfile")
+	flag.StringVar(&kubeconfig, "kubeconfig", configPath(), "absolute path to the kubeconfig file")
 	flag.Parse()
 
-	Exec(kubeconfig)
+	Exec(kubeconfig, isLive, podfile)
 }
 
-func homeDir() string {
+func configPath() string {
 	usr, err := user.Current()
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return ""
 	}
-	return usr.HomeDir
+	return filepath.Join(usr.HomeDir, ".kube", "config")
 }
