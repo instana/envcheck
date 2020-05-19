9edb9c3efff4345c70d927ace625d232304cbe08
Nathan Fisher <nfisher@junctionbox.ca>
2020-05-18 23:24:06 -0300

    Add test for cluster.Info.Apply

--- cluster/info_test.go
+++ cluster/info_test.go
@@ -2,8 +2,36 @@ package cluster_test
 
 import (
 	"testing"
+
+	"github.com/instana/envcheck/cluster"
 )
 
-func Test_wip(t *testing.T) {
-	t.Skip("write a test")
+func Test_Apply(t *testing.T) {
+	testCases := map[string]struct {
+		info  cluster.Info
+		count int
+	}{
+		"empty":   {info: cluster.Info{}, count: 0},
+		"one pod": {info: cluster.Info{Pods: []cluster.PodInfo{{}}}, count: 1},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			c := &counter{}
+			tc.info.Apply(c)
+			if c.count != tc.count {
+				t.Errorf("count=%v, want %v", c.count, tc.count)
+			}
+		})
+	}
+}
+
+type counter struct {
+	count int
+}
+
+func (c *counter) Each(_ cluster.PodInfo) {
+	c.count++
 }
