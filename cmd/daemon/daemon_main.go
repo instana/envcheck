a1672a611cc5d5576b7b0b8f6eb3f48136db9fdd
Nathan Fisher <nfisher@junctionbox.ca>
2020-07-14 12:58:44 -0300

    Add Kube API service host and port to daemon

--- cmd/daemon/daemon_main.go
+++ cmd/daemon/daemon_main.go
@@ -21,12 +21,13 @@ func Exec(address string, info DownwardInfo) error {
 	log.SetFlags(log.LUTC | log.Lmsgprefix | log.LstdFlags)
 	log.SetPrefix(fmt.Sprintf("pod=%s/%s ", info.Namespace, info.Name))
 
-	log.Printf("listen=%s podIP=%s nodeIP=%s daemon=%s", address, info.PodIP, info.NodeIP, Revision)
+	log.Printf("listen=%s podIP=%s nodeIP=%s kubeApi=%s daemon=%s", address, info.PodIP, info.NodeIP, info.KubeAPI, Revision)
 	publish("address", address)
 	publish("name", info.Name)
 	publish("namespace", info.Namespace)
 	publish("nodeIP", info.NodeIP)
 	publish("podIP", info.PodIP)
+	publish("kubeAPI", info.KubeAPI)
 
 	ifs, err := network.MapInterfaces()
 	if err != nil {
@@ -53,6 +54,8 @@ func PingHandler(info DownwardInfo) func(w http.ResponseWriter, r *http.Request)
 }
 
 func main() {
+	var apiHost string
+	var apiPort string
 	var address string
 	var downward DownwardInfo
 
@@ -61,7 +64,11 @@ func main() {
 	flag.StringVar(&downward.NodeIP, "nodeip", os.Getenv("NODEIP"), "node IP this service is running on.")
 	flag.StringVar(&downward.PodIP, "podip", os.Getenv("PODIP"), "pod IP this service is running on.")
 	flag.StringVar(&address, "address", os.Getenv("ADDRESS"), "listening address for this service to bind on.")
+	flag.StringVar(&apiHost, "kubehost", os.Getenv("KUBERNETES_SERVICE_HOST"), "kube api host")
+	flag.StringVar(&apiPort, "kubeport", os.Getenv("KUBERNETES_SERVICE_PORT"), "kube api port")
 	flag.Parse()
+
+	downward.KubeAPI = fmt.Sprintf("%s:%s", apiHost, apiPort)
 
 	err := Exec(address, downward)
 
@@ -81,4 +88,5 @@ type DownwardInfo struct {
 	Namespace string
 	NodeIP    string
 	PodIP     string
+	KubeAPI   string
 }
